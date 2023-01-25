9c9443d189c3cb1444d6df588bc1e8523282d397
Ludielly Silveira <ludysilveira02@gmail.com>
2023-01-25 12:29:24 -0300

    termino da parte 3: fundamentos da POO

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { Personagem } from "./modules/personagem.js"
 import { PersonagemView } from "./components/personagem-view.js"
 import { Mago } from "./modules/magos.js"
+import { Arqueiro } from "./modules/arqueiro.js"
+import { ArqueiroMago } from "./modules/arqueiro-mago.js"
 
-const magoJoao = new Mago('Joao', 7, 'fogo', 8, 6)
 const magaMaria = new Mago('Maria', 5, 'gelo', 7, 10)
+const arqueiroJose = new Arqueiro('Jose', 6, 7)
+const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
 
-const personagens = [magoJoao, magaMaria]
+const personagens = [magaMaria, arqueiroJose, arqueiroMagoChico]
 
 new PersonagemView(personagens).render()
